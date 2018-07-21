63324aef0d816110bc92efd505ba3f7f5021b8ca
Samson Gudise <samsongudise@gmail.com>
2018-07-21 14:06:59 -0400

    Procs table now include hostid

--- populate.sh
+++ populate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+hostid=$(echo `hostname`)
 while read pid name uptime l_ip l_port r_ip r_port
 do
-echo $name $pid
-./ifxs test insert uptime=$uptime,l_ip=$l_ip,l_port=$l_port,r_ip=$r_ip,r_port=$r_port,ts=current into Procs where "pid=$pid&name=$name"
+echo $name $pid $hostid
+./ifxs test insert uptime=$uptime,l_ip=$l_ip,l_port=$l_port,r_ip=$r_ip,r_port=$r_port,ts=current into Procs where "pid=$pid&name=$name&hostid=$hostid"
 done
 exit 0
